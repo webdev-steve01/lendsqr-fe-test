eff888d44060cb2309ee48c562d52e56ae96db32
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-07-30 17:33:26 +0100

    update: change user fetching URL from localhost to production endpoint

--- components/ui/sections/Dashboard.tsx
+++ components/ui/sections/Dashboard.tsx
@@ -10,7 +10,7 @@ import UserTable from "../tables/UserTable";
 
 function Dashboard() {
   const { data, loading, error } = useFetch<User[]>(
-    "http://localhost:3000/api/users"
+    "https://osesojeh-stephen-sylvesterpaul-lend.vercel.app/api/users"
   );
 
   useEffect(() => {
