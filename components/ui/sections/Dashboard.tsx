85bb52be9cb649982b5124fc358402d061142925
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-07-31 02:13:51 +0100

    fix: update user fetching URL to use environment variable; enhance dashboard styles for better responsiveness

--- components/ui/sections/Dashboard.tsx
+++ components/ui/sections/Dashboard.tsx
@@ -10,7 +10,7 @@ import UserTable from "../tables/UserTable";
 
 function Dashboard() {
   const { data, loading, error } = useFetch<User[]>(
-    `http://localhost:3000/api/users`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/users`
   );
 
   useEffect(() => {
